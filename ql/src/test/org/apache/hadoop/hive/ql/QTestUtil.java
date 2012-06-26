0eccef3250f8451a028790621c3253329731ab97
Zhenxiao Luo <zhenxiao@cloudera.com>
2012-06-26 12:10:17 -0700

    CLOUDERA-BUILD. Fix fs_default_name1.q and fs_default_name2.q on cdh4-0.9.0 for both MR1 and MR2

--- ql/src/test/org/apache/hadoop/hive/ql/QTestUtil.java
+++ ql/src/test/org/apache/hadoop/hive/ql/QTestUtil.java
@@ -991,7 +991,6 @@ public class QTestUtil {
         ".*transient_lastDdlTime.*",
         ".*last_modified_.*",
         ".*java.lang.RuntimeException.*",
-        ".*java.lang.IllegalArgumentException.*",
         ".*at org.*",
         ".*at sun.*",
         ".*at java.*",
