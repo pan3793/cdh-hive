cc3c1c3a56caf3b054e0dde6434fbdeffbd17af6
xzhang <xzhang@xzdt.(none)>
2013-06-07 16:46:07 -0700

    Revert "CDH-10427 Unable to create a table from existing file if file path has spaces"
    This reverts commit 554ef34f2b338bbd17851261343d4857f718b55c to allow time to investigate the
    testcase failure.

--- ql/src/java/org/apache/hadoop/hive/ql/parse/LoadSemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/LoadSemanticAnalyzer.java
@@ -27,8 +27,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.antlr.runtime.tree.Tree;
-import org.apache.commons.httpclient.URIException;
-import org.apache.commons.httpclient.util.URIUtil;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
@@ -86,7 +84,7 @@ public class LoadSemanticAnalyzer extends BaseSemanticAnalyzer {
     // directory
     if (!path.startsWith("/")) {
       if (isLocal) {
-        path = URIUtil.decode( new Path(System.getProperty("user.dir"), path).toUri().toString() );
+        path = new Path(System.getProperty("user.dir"), path).toUri().toString();
       } else {
         path = new Path(new Path("/user/" + System.getProperty("user.name")),
           path).toString();
@@ -238,13 +236,8 @@ public class LoadSemanticAnalyzer extends BaseSemanticAnalyzer {
       // that's just a test case.
       String copyURIStr = ctx.getExternalTmpFileURI(toURI);
       URI copyURI = URI.create(copyURIStr);
-      try {
-        rTask = TaskFactory.get(new CopyWork(URIUtil.decode(fromURI.toString()), copyURIStr),
-            conf);
-      } catch (URIException e) {
-        throw new SemanticException(ErrorMsg.INVALID_PATH.getMsg(fromTree, e
-            .getMessage()), e);
-      }
+      rTask = TaskFactory.get(new CopyWork(fromURI.toString(), copyURIStr),
+          conf);
       fromURI = copyURI;
     }
 
@@ -273,14 +266,8 @@ public class LoadSemanticAnalyzer extends BaseSemanticAnalyzer {
     }
 
 
-    LoadTableDesc loadTableWork;
-    try {
-      loadTableWork = new LoadTableDesc(URIUtil.decode(fromURI.toString()),
-          loadTmpPath, Utilities.getTableDesc(ts.tableHandle), partSpec, isOverWrite);
-    } catch (URIException e1) {
-      throw new SemanticException(ErrorMsg.INVALID_PATH.getMsg(fromTree, e1
-          .getMessage()), e1);
-    }
+    LoadTableDesc loadTableWork = new LoadTableDesc(fromURI.toString(),
+        loadTmpPath, Utilities.getTableDesc(ts.tableHandle), partSpec, isOverWrite);
 
     Task<? extends Serializable> childTask = TaskFactory.get(new MoveWork(getInputs(),
         getOutputs(), loadTableWork, null, true), conf);
