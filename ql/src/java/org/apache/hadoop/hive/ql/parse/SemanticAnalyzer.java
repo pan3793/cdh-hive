b81f478f5b765556dd132606a04a8929306a8edb
Prasad Mujumdar <prasadm@cloudera.com>
2013-06-21 11:27:06 -0700

    HIVE-4973. Compiler should captures UDF as part of read entities

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -435,6 +435,9 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
         if(FunctionRegistry.impliesOrder(functionName)) {
           throw new SemanticException(ErrorMsg.MISSING_OVER_CLAUSE.getMsg(functionName));
         }
+        if (conf.getBoolVar(ConfVars.HIVE_EXTENDED_ENITITY_CAPTURE)) {
+          getInputs().add(new ReadEntity(FunctionRegistry.getFunctionInfo(functionName)));
+        }
         if (FunctionRegistry.getGenericUDAFResolver(functionName) != null) {
           if(containsLeadLagUDF(expressionTree)) {
             throw new SemanticException(ErrorMsg.MISSING_OVER_CLAUSE.getMsg(functionName));
@@ -2490,6 +2493,9 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
       FunctionInfo fi = FunctionRegistry.getFunctionInfo(funcName);
       if (fi != null) {
         genericUDTF = fi.getGenericUDTF();
+        if (conf.getBoolVar(ConfVars.HIVE_EXTENDED_ENITITY_CAPTURE)) {
+          getInputs().add(new ReadEntity(fi));
+        }
       }
       isUDTF = (genericUDTF != null);
       if (isUDTF) {
