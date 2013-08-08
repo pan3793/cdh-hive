da92f56871042877a2a7acc5a57c92ef4e00629a
Prasad Mujumdar <prasadm@cloudera.com>
2013-08-08 01:12:42 -0700

    CDH-12525: Backport HIVE-4233: The TGT gotten from class 'CLIService' should be renewed on time (Thejas M Nair via Gunther Hagleitner

--- shims/src/common/java/org/apache/hadoop/hive/shims/HadoopShims.java
+++ shims/src/common/java/org/apache/hadoop/hive/shims/HadoopShims.java
@@ -322,6 +322,13 @@ public interface HadoopShims {
   public void loginUserFromKeytab(String principal, String keytabFile) throws IOException;
 
   /**
+   * Perform kerberos re-login using the given principal and keytab, to renew
+   * the credentials
+   * @throws IOException
+   */
+  public void reLoginUserFromKeytab() throws IOException;
+
+  /**
    * Create the proxy ugi for the given userid
    * @param userName
    * @return
