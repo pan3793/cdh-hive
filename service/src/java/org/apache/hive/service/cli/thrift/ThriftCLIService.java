7d0b32b983d1109d50d28e58e21a96f6f98c22e4
Chaoyu Tang <ctang@cloudera.com>
2014-01-20 20:06:49 -0500

    PROPOSED CDH-16179: HIVE-6245 HS2 creates DBs/Tables with wrong ownership when HMS setugi is true

--- service/src/java/org/apache/hive/service/cli/thrift/ThriftCLIService.java
+++ service/src/java/org/apache/hive/service/cli/thrift/ThriftCLIService.java
@@ -27,6 +27,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
+import org.apache.hadoop.hive.ql.metadata.Hive;
 import org.apache.hadoop.hive.shims.ShimLoader;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hive.service.AbstractService;
@@ -141,10 +142,17 @@ public class ThriftCLIService extends AbstractService implements TCLIService.Ifa
       if (cliService.getHiveConf().
           getBoolVar(HiveConf.ConfVars.HIVE_SERVER2_KERBEROS_IMPERSONATION)) {
         String delegationTokenStr = null;
-        try {
-          delegationTokenStr = cliService.getDelegationTokenFromMetaStore(userName);
-        } catch (UnsupportedOperationException e) {
-          // The delegation token is not applicable in the given deployment mode
+        
+        if (!cliService.getHiveConf().getBoolVar(HiveConf.ConfVars.METASTORE_USE_THRIFT_SASL)) {
+          if (cliService.getHiveConf().getBoolVar(HiveConf.ConfVars.METASTORE_EXECUTE_SET_UGI)) {
+            Hive.closeCurrent();
+          }
+        } else {
+          try {
+            delegationTokenStr = cliService.getDelegationTokenFromMetaStore(userName);
+          } catch (UnsupportedOperationException e) {
+            // The delegation token is not applicable in the given deployment mode
+          }
         }
         sessionHandle = cliService.openSessionWithImpersonation(userName, req.getPassword(),
               req.getConfiguration(), delegationTokenStr);
