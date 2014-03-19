64917ba4abf5f817b01ac4b29437eea74404e495
Brock Noland <brock@apache.org>
2014-03-18 18:16:59 -0700

    FIXUP: CDH-17479 - Backport HIVE-5946 - DDL authorization task factory should be better tested

--- ql/src/test/org/apache/hadoop/hive/ql/parse/authorization/TestHiveAuthorizationTaskFactoryImpl.java
+++ ql/src/test/org/apache/hadoop/hive/ql/parse/authorization/TestHiveAuthorizationTaskFactoryImpl.java
@@ -50,7 +50,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-public class TestDefaultHiveAuthorizationTaskFactory {
+public class TestHiveAuthorizationTaskFactoryImpl {
 
   private static final String SELECT = "SELECT";
   private static final String DB = "default";
