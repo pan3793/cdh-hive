7af9de2e45ba503a6b1b962e720e13c3a413f1ee
Mohit Sabharwal <mohit@cloudera.com>
2014-07-29 14:45:52 -0700

    CDH-20559: Add hadoop config names for MR1

--- shims/0.23/src/main/java/org/apache/hadoop/hive/shims/Hadoop23Shims.java
+++ shims/0.23/src/main/java/org/apache/hadoop/hive/shims/Hadoop23Shims.java
@@ -770,18 +770,34 @@ public class Hadoop23Shims extends HadoopShimsSecure {
   @Override
   public Map<String, String> getHadoopConfNames() {
     Map<String, String> ret = new HashMap<String, String>();
-    ret.put("HADOOPFS", "fs.defaultFS");
-    ret.put("HADOOPMAPFILENAME", "mapreduce.map.input.file");
-    ret.put("HADOOPMAPREDINPUTDIR", "mapreduce.input.fileinputformat.inputdir");
-    ret.put("HADOOPMAPREDINPUTDIRRECURSIVE", "mapreduce.input.fileinputformat.input.dir.recursive");
-    ret.put("MAPREDMAXSPLITSIZE", "mapreduce.input.fileinputformat.split.maxsize");
-    ret.put("MAPREDMINSPLITSIZE", "mapreduce.input.fileinputformat.split.minsize");
-    ret.put("MAPREDMINSPLITSIZEPERNODE", "mapreduce.input.fileinputformat.split.minsize.per.node");
-    ret.put("MAPREDMINSPLITSIZEPERRACK", "mapreduce.input.fileinputformat.split.minsize.per.rack");
-    ret.put("HADOOPNUMREDUCERS", "mapreduce.job.reduces");
-    ret.put("HADOOPJOBNAME", "mapreduce.job.name");
-    ret.put("HADOOPSPECULATIVEEXECREDUCERS", "mapreduce.reduce.speculative");
-    ret.put("MAPREDSETUPCLEANUPNEEDED", "mapreduce.job.committer.setup.cleanup.needed");
+    boolean mr2 = isMR2(new Configuration());
+    if (mr2) {
+      ret.put("HADOOPFS", "fs.defaultFS");
+      ret.put("HADOOPMAPFILENAME", "mapreduce.map.input.file");
+      ret.put("HADOOPMAPREDINPUTDIR", "mapreduce.input.fileinputformat.inputdir");
+      ret.put("HADOOPMAPREDINPUTDIRRECURSIVE", "mapreduce.input.fileinputformat.input.dir.recursive");
+      ret.put("MAPREDMAXSPLITSIZE", "mapreduce.input.fileinputformat.split.maxsize");
+      ret.put("MAPREDMINSPLITSIZE", "mapreduce.input.fileinputformat.split.minsize");
+      ret.put("MAPREDMINSPLITSIZEPERNODE", "mapreduce.input.fileinputformat.split.minsize.per.node");
+      ret.put("MAPREDMINSPLITSIZEPERRACK", "mapreduce.input.fileinputformat.split.minsize.per.rack");
+      ret.put("HADOOPNUMREDUCERS", "mapreduce.job.reduces");
+      ret.put("HADOOPJOBNAME", "mapreduce.job.name");
+      ret.put("HADOOPSPECULATIVEEXECREDUCERS", "mapreduce.reduce.speculative");
+      ret.put("MAPREDSETUPCLEANUPNEEDED", "mapreduce.job.committer.setup.cleanup.needed");
+    } else {
+      ret.put("HADOOPFS", "fs.default.name");
+      ret.put("HADOOPMAPFILENAME", "map.input.file");
+      ret.put("HADOOPMAPREDINPUTDIR", "mapred.input.dir");
+      ret.put("HADOOPMAPREDINPUTDIRRECURSIVE", "mapred.input.dir.recursive");
+      ret.put("MAPREDMAXSPLITSIZE", "mapred.max.split.size");
+      ret.put("MAPREDMINSPLITSIZE", "mapred.min.split.size");
+      ret.put("MAPREDMINSPLITSIZEPERRACK", "mapred.min.split.size.per.rack");
+      ret.put("MAPREDMINSPLITSIZEPERNODE", "mapred.min.split.size.per.node");
+      ret.put("HADOOPNUMREDUCERS", "mapred.reduce.tasks");
+      ret.put("HADOOPJOBNAME", "mapred.job.name");
+      ret.put("HADOOPSPECULATIVEEXECREDUCERS", "mapred.reduce.tasks.speculative.execution");
+      ret.put("MAPREDSETUPCLEANUPNEEDED", "mapred.committer.job.setup.cleanup.needed");
+    }
     ret.put("MAPREDTASKCLEANUPNEEDED", "mapreduce.job.committer.task.cleanup.needed");
     return ret;
  }
