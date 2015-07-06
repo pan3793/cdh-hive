6c47f9ef0cd1904cdbe2a7663834dbed62a6d952
Chao Sun <sunchao@apache.org>
2015-07-06 16:09:22 -0700

    Revert "HIVE-10573 - Improve Hive service for connection (Chao, reviewed by Thejas)"
    Revert because this commit doesn't include unit test.
    
    This reverts commit 82eec29d1f6b472bd5bd290f4de34a9cc0ca604a.

--- service/src/java/org/apache/hive/service/auth/LdapAuthenticationProviderImpl.java
+++ service/src/java/org/apache/hive/service/auth/LdapAuthenticationProviderImpl.java
@@ -53,11 +53,6 @@ public class LdapAuthenticationProviderImpl implements PasswdAuthenticationProvi
       user  = user + "@" + ldapDomain;
     }
 
-    if (password == null || password.isEmpty() || password.getBytes()[0] == 0) {
-      throw new AuthenticationException("Error validating LDAP user:" +
-          " a null or blank password has been provided");
-    }
-
     // setup the security principal
     String bindDN;
     if (baseDN == null) {
