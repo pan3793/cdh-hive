18cefdb7d904ce70b3b6928cc5e3028ff2729ee9
Yongzhi Chen <ychena@apache.org>
2016-05-16 15:40:22 -0400

    CDH-40408: HIVE-13736: View's input/output formats are TEXT by default. (Yongzhi Chen, reviewed by Chaoyu Tang)
    Conflicts:
    	ql/src/java/org/apache/hadoop/hive/ql/exec/DDLTask.java
    	ql/src/test/results/clientpositive/llap/selectDistinctStar.q.out
    
    Change-Id: I6d20a2b9f2435e2f162e925dffc2379427582766
    (cherry picked from commit 11280d888cda48942cb068e90960a049545f5156)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/DDLTask.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/DDLTask.java
@@ -4162,6 +4162,12 @@ public class DDLTask extends Task<DDLWork> implements Serializable {
         oldview.getTTable().getParameters().putAll(crtView.getTblProps());
       }
       oldview.setPartCols(crtView.getPartCols());
+      if (crtView.getInputFormat() != null) {
+        oldview.setInputFormatClass(crtView.getInputFormat());
+      }
+      if (crtView.getOutputFormat() != null) {
+        oldview.setOutputFormatClass(crtView.getOutputFormat());
+      }
       oldview.checkValidity();
       try {
         db.alterTable(crtView.getViewName(), oldview);
@@ -4187,6 +4193,13 @@ public class DDLTask extends Task<DDLWork> implements Serializable {
 
       if (crtView.getPartCols() != null) {
         tbl.setPartCols(crtView.getPartCols());
+      }
+
+      if (crtView.getInputFormat() != null) {
+        tbl.setInputFormatClass(crtView.getInputFormat());
+      }
+      if (crtView.getOutputFormat() != null) {
+        tbl.setOutputFormatClass(crtView.getOutputFormat());
       }
 
       db.createTable(tbl, crtView.getIfNotExists());
