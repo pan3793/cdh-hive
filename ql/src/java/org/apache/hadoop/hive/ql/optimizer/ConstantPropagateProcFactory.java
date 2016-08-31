539d66ecb7707320322c597e771ff230cd302dcc
stakiar <stakiar@cloudera.com>
2016-08-30 18:22:40 -0700

    CLOUDERA-BUILD: CDH-39679: Hive sub-queries drop select expressions when alias = column name and constant propagation occurs
    * Note this commit does not have a corresponding upstream issue
    * While HIVE-13602 does fix this bug upstream, the patch for the JIRA has too many irrelevant changes to the bug reported in CDH-39679
    * The JIRA has more details on why we decided to push this into CDH, rather than backporting the entire change
    * This fix is based on a portion of the patch available in HIVE-13602, specifically the portion relevant to ConstantPropagateProcFactory
    * The bug only occurs when the Hive query has a sub-query where the column name is aliased to its original name and a predicate in the WHERE clause triggers expression folding via the constant propagation rule in the RBO
    * More details on the fix can be found in the CDH JIRA
    
    Change-Id: I4b6d6286a9124883cf638a539e46f37dc691f22d
    (cherry picked from commit ab6f1f7e9ff9308c6fed00ad279a32ebf23f63ce)

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/ConstantPropagateProcFactory.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/ConstantPropagateProcFactory.java
@@ -48,6 +48,7 @@ import org.apache.hadoop.hive.ql.lib.Node;
 import org.apache.hadoop.hive.ql.lib.NodeProcessor;
 import org.apache.hadoop.hive.ql.lib.NodeProcessorCtx;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.metadata.VirtualColumn;
 import org.apache.hadoop.hive.ql.parse.SemanticException;
 import org.apache.hadoop.hive.ql.plan.DynamicPartitionCtx;
 import org.apache.hadoop.hive.ql.plan.ExprNodeColumnDesc;
@@ -877,6 +878,12 @@ public final class ConstantPropagateProcFactory {
             }
           }
           colList.set(i, newCol);
+          if (newCol instanceof ExprNodeConstantDesc && op.getSchema() != null) {
+            ColumnInfo colInfo = op.getSchema().getSignature().get(i);
+            if (!VirtualColumn.isVirtualColumnBasedOnAlias(colInfo)) {
+              constants.put(colInfo, newCol);
+            }
+          }
           if (columnExprMap != null) {
             columnExprMap.put(columnNames.get(i), newCol);
           }
