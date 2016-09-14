d5f80ef65ac40895623bbefbd5d04ba91ee95e8a
Ashutosh Chauhan <hashutosh@apache.org>
2016-09-14 16:41:07 -0700

    HIVE-14762 : Add logging while removing scratch space (Ashutosh Chauhan via Pengcheng Xiong)
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

--- ql/src/java/org/apache/hadoop/hive/ql/Context.java
+++ ql/src/java/org/apache/hadoop/hive/ql/Context.java
@@ -98,7 +98,7 @@ public class Context {
   protected int tryCount = 0;
   private TokenRewriteStream tokenRewriteStream;
 
-  private String executionId;
+  private final String executionId;
 
   // List of Locks for this query
   protected List<HiveLock> hiveLocks;
@@ -126,7 +126,7 @@ public class Context {
   private final String stagingDir;
 
   private Heartbeater heartbeater;
-  
+
   private boolean skipTableMasking;
 
   public Context(Configuration conf) throws IOException {
@@ -398,6 +398,7 @@ public class Context {
       try {
         Path p = entry.getValue();
         FileSystem fs = p.getFileSystem(conf);
+        LOG.debug("Deleting scratch dir: {}",  p);
         fs.delete(p, true);
         fs.cancelDeleteOnExit(p);
       } catch (Exception e) {
@@ -540,6 +541,7 @@ public class Context {
     if (resDir != null) {
       try {
         FileSystem fs = resDir.getFileSystem(conf);
+        LOG.debug("Deleting result dir: {}",  resDir);
         fs.delete(resDir, true);
       } catch (IOException e) {
         LOG.info("Context clear error: " + StringUtils.stringifyException(e));
@@ -549,6 +551,7 @@ public class Context {
     if (resFile != null) {
       try {
         FileSystem fs = resFile.getFileSystem(conf);
+        LOG.debug("Deleting result file: {}",  resFile);
         fs.delete(resFile, false);
       } catch (IOException e) {
         LOG.info("Context clear error: " + StringUtils.stringifyException(e));
