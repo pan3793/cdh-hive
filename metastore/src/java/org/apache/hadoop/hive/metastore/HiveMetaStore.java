aeda860ff02aee2912db7c328b9ca36a45b1946d
stakiar <stakiar@cloudera.com>
2016-09-14 12:04:49 -0700

    CDH-44357: Reverting CDH-36924 / HIVE-14195
    Reason: The JIRA broke API compatibility of HMS's getFunction() method; if a function
    no longer exists a NoSuchObjectException is thrown rather than a MetaException
    
    Revert "CDH-36924 HIVE-14195 : HiveMetaStoreClient getFunction() does not throw NoSuchObjectException (Peter Vary, reviewed by Sergey Shelukhin)"
    
    This reverts commit b9e94a9d620a0f5844f5ab75651b55445d12243a.
    
    Change-Id: I6f897b81ca04b3bb7d42e921a3bf646bfd9155b8

--- metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
+++ metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
@@ -5753,9 +5753,6 @@ public class HiveMetaStore extends ThriftHiveMetastore {
           throw new NoSuchObjectException(
               "Function " + dbName + "." + funcName + " does not exist");
         }
-      } catch (NoSuchObjectException e) {
-        ex = e;
-        rethrowException(e);
       } catch (Exception e) {
         ex = e;
         throw newMetaException(e);
