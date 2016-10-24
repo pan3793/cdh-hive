2d0aae5ff75db3b57278c321df027e60b3cb57cd
Siddharth Seth <sseth@apache.org>
2016-10-24 23:50:41 +0200

    CDH-74157: HIVE-14391: TestAccumuloCliDriver is not executed during precommit tests. (Peter Vary, reviewed by Siddharth Seth)
    (cherry picked from commit ad5d8169752489d27d33afe43d263dec09bb667d)
    
    Change-Id: I12706fd30bb9474222e54a60c90ed7786f331af3

--- accumulo-handler/src/java/org/apache/hadoop/hive/accumulo/predicate/PrimitiveComparisonFilter.java
+++ accumulo-handler/src/java/org/apache/hadoop/hive/accumulo/predicate/PrimitiveComparisonFilter.java
@@ -56,7 +56,7 @@ public class PrimitiveComparisonFilter extends WholeRowIterator {
   @SuppressWarnings("unused")
   private static final Logger log = LoggerFactory.getLogger(PrimitiveComparisonFilter.class);
 
-  public static final String FILTER_PREFIX = "accumulo.filter.compare.iterator.";
+  public static final String FILTER_PREFIX = "accumulo_filter_compare_iterator_";
   public static final String P_COMPARE_CLASS = "accumulo.filter.iterator.p.compare.class";
   public static final String COMPARE_OPT_CLASS = "accumulo.filter.iterator.compare.opt.class";
   public static final String CONST_VAL = "accumulo.filter.iterator.const.val";
