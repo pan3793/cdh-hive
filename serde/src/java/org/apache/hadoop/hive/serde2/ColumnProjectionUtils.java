c5cdd0a66f0889a8ee108a87d17126455bd052a9
Aihua Xu <aihuaxu@apache.org>
2017-04-07 09:57:25 -0400

    CDH-52274: HIVE-16291: Hive fails when unions a parquet table with itself (Yibing Shi, reviewed by Ashutosh Chauhan & Aihua Xu)
    Change-Id: I26e3086157809d0a619a46c1b87de4cff93e2b04

--- serde/src/java/org/apache/hadoop/hive/serde2/ColumnProjectionUtils.java
+++ serde/src/java/org/apache/hadoop/hive/serde2/ColumnProjectionUtils.java
@@ -23,6 +23,7 @@ import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.util.StringUtils;
+import org.apache.hive.common.util.HiveStringUtils;
 
 /**
  * ColumnProjectionUtils.
@@ -95,10 +96,7 @@ public final class ColumnProjectionUtils {
   public static void appendReadColumns(Configuration conf, List<Integer> ids) {
     String id = toReadColumnIDString(ids);
     String old = conf.get(READ_COLUMN_IDS_CONF_STR, null);
-    String newConfStr = id;
-    if (old != null && !old.isEmpty()) {
-      newConfStr = newConfStr + StringUtils.COMMA_STR + old;
-    }
+    String newConfStr = HiveStringUtils.joinIgnoringEmpty(new String[] {id, old}, StringUtils.COMMA);
     setReadColumnIDConf(conf, newConfStr);
     // Set READ_ALL_COLUMNS to false
     conf.setBoolean(READ_ALL_COLUMNS, false);
