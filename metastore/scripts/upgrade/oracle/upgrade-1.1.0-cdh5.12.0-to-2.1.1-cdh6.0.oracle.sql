972b250b259fcc3dc8d600b5bb64537cf0a14ff7
Vihang Karajgaonkar <vihang@cloudera.com>
2017-05-23 13:39:16 -0700

    CLOUDERA-BUILD : CDH-54138 : Enable uuid patch with schematool
    Change-Id: I0ee033ac564b1adfee0449dcf791c6d42bafff0b
    (cherry picked from commit 5d400b40c6af850a0e707c0bc9d28c1684438570)

--- metastore/scripts/upgrade/oracle/upgrade-1.1.0-cdh5.12.0-to-2.1.1-cdh6.0.oracle.sql
+++ metastore/scripts/upgrade/oracle/upgrade-1.1.0-cdh5.12.0-to-2.1.1-cdh6.0.oracle.sql
@@ -1,6 +1,4 @@
 SELECT 'Upgrading MetaStore schema from 1.1.0 to 2.1.1' AS Status from dual;
 
-@041-HIVE-16556.oracle.sql;
-
-UPDATE VERSION SET SCHEMA_VERSION='2.1.1', VERSION_COMMENT='Hive release version 2.1.1 for CDH 6.0' where VER_ID=1;
+UPDATE VERSION SET SCHEMA_VERSION='2.1.1-cdh6.0', VERSION_COMMENT='Hive release version 2.1.1 for CDH 6.0' where VER_ID=1;
 SELECT 'Finished upgrading MetaStore schema from 1.1.0 to 2.1.1' AS Status from dual;
