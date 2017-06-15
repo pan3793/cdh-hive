apache/hive

CDH-53836: HIVE-14389: Beeline should not output query and prompt to stdout (Vihang Karajgaonkar, reviewed by Peter Vary, Thejas M Nair and Yongzhi Chen)            Contains the test framework changes from: HIVE-15900 : Beeline prints tez job progress in stdout instead of stderr (Thejas Nair, reviewed by Daniel Dai, Anishek Agarwal)
(cherry picked from commit 60a36d124d437bea123c94d50ccb171e24ff2f3d)
(cherry picked from commit dd2697c00dffe17699f19f8accfbf5c14bd07219)

Change-Id: I7087a1824f0305904131f983678a48bd04c3c05b

beeline/src/test/org/apache/hive/beeline/cli/TestHiveCli.java
@@ -112,7 +112,7 @@ public class TestHiveCli {
 
   @Test
   public void testSetPromptValue() {
-    verifyCMD("set hive.cli.prompt=MYCLI;SHOW\nTABLES;", "MYCLI> ", os, null,
+    verifyCMD("set hive.cli.prompt=MYCLI;SHOW\nTABLES;", "MYCLI> ", errS, null,
         ERRNO_OK, true);
   }
 
@@ -213,21 +213,21 @@ public class TestHiveCli {
   public void testUseCurrentDB1() {
     verifyCMD(
         "create database if not exists testDB; set hive.cli.print.current.db=true;use testDB;\n"
-            + "use default;drop if exists testDB;", "hive (testDB)>", os, null, ERRNO_OK, true);
+            + "use default;drop if exists testDB;", "hive (testDB)>", errS, null, ERRNO_OK, true);
   }
 
   @Test
   public void testUseCurrentDB2() {
     verifyCMD(
         "create database if not exists testDB; set hive.cli.print.current.db=true;use\ntestDB;\nuse default;drop if exists testDB;",
-        "hive (testDB)>", os, null, ERRNO_OK, true);
+        "hive (testDB)>", errS, null, ERRNO_OK, true);
   }
 
   @Test
   public void testUseCurrentDB3() {
     verifyCMD(
         "create database if not exists testDB; set hive.cli.print.current.db=true;use  testDB;\n"
-            + "use default;drop if exists testDB;", "hive (testDB)>", os, null, ERRNO_OK, true);
+            + "use default;drop if exists testDB;", "hive (testDB)>", errS, null, ERRNO_OK, true);
   }
 
   @Test

pan3793/cdh-hive

(cherry picked from commit 5eecf810a5689c2014608586fefc5de3ccf1f3f1)

beeline/src/test/org/apache/hive/beeline/cli/TestHiveCli.java
@@ -113,7 +113,7 @@ public class TestHiveCli {
 
   @Test
   public void testSetPromptValue() {
-    verifyCMD("set hive.cli.prompt=MYCLI;SHOW\nTABLES;", "MYCLI> ", os, null,
+    verifyCMD("set hive.cli.prompt=MYCLI;SHOW\nTABLES;", "MYCLI> ", errS, null,
         ERRNO_OK, true);
   }
 
@@ -222,21 +222,21 @@ public class TestHiveCli {
   public void testUseCurrentDB1() {
     verifyCMD(
         "create database if not exists testDB; set hive.cli.print.current.db=true;use testDB;\n"
-            + "use default;drop if exists testDB;", "hive (testDB)>", os, null, ERRNO_OTHER, true);
+            + "use default;drop if exists testDB;", "hive (testDB)>", errS, null, ERRNO_OTHER, true);
   }
 
   @Test
   public void testUseCurrentDB2() {
     verifyCMD(
         "create database if not exists testDB; set hive.cli.print.current.db=true;use\ntestDB;\nuse default;drop if exists testDB;",
-        "hive (testDB)>", os, null, ERRNO_OTHER, true);
+        "hive (testDB)>", errS, null, ERRNO_OTHER, true);
   }
 
   @Test
   public void testUseCurrentDB3() {
     verifyCMD(
         "create database if not exists testDB; set hive.cli.print.current.db=true;use  testDB;\n"
-            + "use default;drop if exists testDB;", "hive (testDB)>", os, null, ERRNO_OTHER, true);
+            + "use default;drop if exists testDB;", "hive (testDB)>", errS, null, ERRNO_OTHER, true);
   }
 
   @Test