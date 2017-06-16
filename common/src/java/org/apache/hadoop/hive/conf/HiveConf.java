e6fbfc67ad95470c77bfbc4fae888213bfc39fc1
Rui Li <lirui@apache.org>
2017-06-16 10:05:46 +0800

    CDH-59936: HIVE-16876: HoS: Make Rpc configs immutable at runtime (Rui reviewed by Xuefu)
    (cherry picked from commit c4382569d7e9b8cd853d44de924bff8c84ac6e94)
    
    Conflicts:
    	common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
    
    Change-Id: I27b9c631556f0d7050f4cd8332e7e0d9d9c63ba2

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -3111,6 +3111,14 @@ public class HiveConf extends Configuration {
     HIVE_CONF_RESTRICTED_LIST("hive.conf.restricted.list",
         "hive.security.authenticator.manager,hive.security.authorization.manager,hive.users.in.admin.role," +
         "hive.server2.xsrf.filter.enabled",
+            "hive.spark.client.connect.timeout," +
+            "hive.spark.client.server.connect.timeout," +
+            "hive.spark.client.channel.log.level," +
+            "hive.spark.client.rpc.max.size," +
+            "hive.spark.client.rpc.threads," +
+            "hive.spark.client.secret.bits," +
+            "hive.spark.client.rpc.server.address," +
+            "hive.spark.client.rpc.server.port",
         "Comma separated list of configuration options which are immutable at runtime"),
     HIVE_CONF_HIDDEN_LIST("hive.conf.hidden.list",
         METASTOREPWD.varname + "," + HIVE_SERVER2_SSL_KEYSTORE_PASSWORD.varname
