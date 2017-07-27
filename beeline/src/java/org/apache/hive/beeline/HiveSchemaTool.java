5b4cc21f613ab5ffa9e2d543a3dc55ba1482145e
Aihua Xu <axu@cloudera.com>
2017-07-27 11:29:16 -0700

    Revert "CDH-51897 HIVE-16912: Improve table validator's performance against Oracle (Naveen Gangam, reviewed by Aihua Xu)"
    This reverts commit b296333f720b63dfc17e94736f7dd4d6fd710ea9.
    
    Change-Id: If8bcdb06019bc2648d0c663dea535bb6bbb63466

--- beeline/src/java/org/apache/hive/beeline/HiveSchemaTool.java
+++ beeline/src/java/org/apache/hive/beeline/HiveSchemaTool.java
@@ -729,7 +729,7 @@ public class HiveSchemaTool {
     try {
       metadata       = conn.getMetaData();
       String[] types = {"TABLE"};
-      rs             = metadata.getTables(null, hmsConn.getSchema(), "%", types);
+      rs             = metadata.getTables(null, null, "%", types);
       String table   = null;
 
       while (rs.next()) {
