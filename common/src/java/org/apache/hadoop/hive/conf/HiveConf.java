52f2e75ea4355768de30eb0fac37bd9a569a38b1
Rui Li <lirui@apache.org>
2017-11-20 10:10:21 +0800

    CDH-65393: HIVE-17964: HoS: some spark configs doesn't require re-creating a session (Rui reviewed by Xuefu)
    (cherry picked from commit 966d2b303b473e46a2877c13a58497b98c0896d5)
    
    Conflicts:
    	common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
    
    Change-Id: I1377121f72b2f511f6d878e69ce6cdae855a1f23

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -90,6 +90,7 @@ public class HiveConf extends Configuration {
   private static final Map<String, ConfVars> metaConfs = new HashMap<String, ConfVars>();
   private final List<String> restrictList = new ArrayList<String>();
   private final Set<String> hiddenSet = new HashSet<String>();
+  private final List<String> rscList = new ArrayList<>();
 
   private Pattern modWhiteListPattern = null;
   private volatile boolean isSparkConfigUpdated = false;
@@ -3171,6 +3172,7 @@ public class HiveConf extends Configuration {
             "hive.spark.client.secret.bits," +
             "hive.spark.client.rpc.server.address," +
             "hive.spark.client.rpc.server.port," +
+            "hive.spark.client.rpc.sasl.mechanisms," +
             "hadoop.bin.path," +
             "yarn.bin.path",
         "Comma separated list of configuration options which are immutable at runtime"),
@@ -3190,6 +3192,11 @@ public class HiveConf extends Configuration {
     HIVE_CONF_INTERNAL_VARIABLE_LIST("hive.conf.internal.variable.list",
         "hive.added.files.path,hive.added.jars.path,hive.added.archives.path",
         "Comma separated list of variables which are used internally and should not be configurable."),
+
+    HIVE_SPARK_RSC_CONF_LIST("hive.spark.rsc.conf.list",
+            SPARK_CLIENT_FUTURE_TIMEOUT.varname,
+        "Comma separated list of variables which are related to remote spark context.\n" +
+            "Changing these variables will result in re-creating the spark session."),
 
     HIVE_QUERY_TIMEOUT_SECONDS("hive.query.timeout.seconds", "0s",
         new TimeValidator(TimeUnit.SECONDS),
@@ -3530,7 +3537,7 @@ public class HiveConf extends Configuration {
       if (sparkMaster != null && sparkMaster.startsWith("yarn")) {
         result = true;
       }
-    } else if (name.startsWith("hive.spark")) { // Remote Spark Context property.
+    } else if (rscList.stream().anyMatch(rscVar -> rscVar.equals(name))) { // Remote Spark Context property.
       result = true;
     } else if (name.equals("mapreduce.job.queuename")) {
       // a special property starting with mapreduce that we would also like to effect if it changes
@@ -3991,6 +3998,7 @@ public class HiveConf extends Configuration {
     setupRestrictList();
     hiddenSet.clear();
     hiddenSet.addAll(HiveConfUtil.getHiddenSet(this));
+    setupRSCList();
   }
 
   /**
@@ -4344,6 +4352,17 @@ public class HiveConf extends Configuration {
     restrictList.add(ConfVars.HIVE_CONF_RESTRICTED_LIST.varname);
     restrictList.add(ConfVars.HIVE_CONF_HIDDEN_LIST.varname);
     restrictList.add(ConfVars.HIVE_CONF_INTERNAL_VARIABLE_LIST.varname);
+    restrictList.add(ConfVars.HIVE_SPARK_RSC_CONF_LIST.varname);
+  }
+
+  private void setupRSCList() {
+    rscList.clear();
+    String vars = this.getVar(ConfVars.HIVE_SPARK_RSC_CONF_LIST);
+    if (vars != null) {
+      for (String var : vars.split(",")) {
+        rscList.add(var.trim());
+      }
+    }
   }
 
   /**
