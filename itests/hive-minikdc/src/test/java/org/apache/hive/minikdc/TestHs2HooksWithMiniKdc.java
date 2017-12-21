1f8590fe1b643fc5dd19fb07a87b601f43a45b9d
Aihua Xu <axu@cloudera.com>
2017-12-21 11:11:12 -0800

    Proposed CDH-62093: HIVE-18327: HS2 integration tests are consistently failing.   Add the test configuration krb5.conf for miniKDC tests and remove the unnecessary HiveConf from MiniKDC constructor.
    Change-Id: I34b629a94c4a662310a6c19be401857fc22da140

--- itests/hive-minikdc/src/test/java/org/apache/hive/minikdc/TestHs2HooksWithMiniKdc.java
+++ itests/hive-minikdc/src/test/java/org/apache/hive/minikdc/TestHs2HooksWithMiniKdc.java
@@ -55,8 +55,8 @@ public class TestHs2HooksWithMiniKdc {
     confOverlay.put(ConfVars.SEMANTIC_ANALYZER_HOOK.varname,
         SemanticAnalysisHook.class.getName());
 
+    miniHiveKdc = MiniHiveKdc.getMiniHiveKdc();
     HiveConf hiveConf = new HiveConf();
-    miniHiveKdc = MiniHiveKdc.getMiniHiveKdc(hiveConf);
     miniHS2 = MiniHiveKdc.getMiniHS2WithKerb(miniHiveKdc, hiveConf);
     miniHS2.start(confOverlay);
   }
