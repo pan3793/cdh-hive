c008daa1e55bb94b5c03853a674a8903ef11c686
Sahil Takiar <stakiar@cloudera.com>
2018-01-03 11:46:53 -0800

    CDH-63427: HIVE-17929: Use sessionId for HoS Remote Driver Client id (Sahil Takiar, reviewed by Rui Li)
    (cherry picked from commit d0156565adec240735850bedc7729fa43cd05460)
    
    Change-Id: I982272c510571c97d8b72b36f747473f746fa9d1

--- spark-client/src/test/java/org/apache/hive/spark/client/TestSparkClient.java
+++ spark-client/src/test/java/org/apache/hive/spark/client/TestSparkClient.java
@@ -29,6 +29,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -273,7 +274,7 @@ public class TestSparkClient {
     SparkClient client = null;
     try {
       test.config(conf);
-      client = SparkClientFactory.createClient(conf, HIVECONF);
+      client = SparkClientFactory.createClient(conf, HIVECONF, UUID.randomUUID().toString());
       test.call(client);
     } finally {
       if (client != null) {
