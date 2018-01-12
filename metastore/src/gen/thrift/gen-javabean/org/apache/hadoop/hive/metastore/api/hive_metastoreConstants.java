76b6c69cf040af706bcf48a8616ea4a7f2af04e7
stakiar <stakiar@cloudera.com>
2018-01-12 12:29:52 -0600

    CLOUDERA-BUILD: CDH-63829: Fix annotation names for HMS Thrift APIs
    Details are in the JIRA. This patch basically changes some
    of the parameters to the Maven Replacer Plugin configured
    in metastore/pom.xml.
    
    This fixes an issue where jacc was no checking for
    incompatible changes to the HMS Thrift APIs.
    
    Change-Id: I6eaaad75c4ec46cd20b9950dff4bd073d96bb61f

--- metastore/src/gen/thrift/gen-javabean/org/apache/hadoop/hive/metastore/api/hive_metastoreConstants.java
+++ metastore/src/gen/thrift/gen-javabean/org/apache/hadoop/hive/metastore/api/hive_metastoreConstants.java
@@ -34,7 +34,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @SuppressWarnings({"cast", "rawtypes", "serial", "unchecked"})
-@org.apache.hadoop.classification.InterfaceAudience.Public @org.apache.hadoop.classification.InterfaceStability.Stable public class hive_metastoreConstants {
+@org.apache.hadoop.hive.common.classification.InterfaceAudience.Public @org.apache.hadoop.hive.common.classification.InterfaceStability.Stable public class hive_metastoreConstants {
 
   public static final String DDL_TIME = "transient_lastDdlTime";
 
