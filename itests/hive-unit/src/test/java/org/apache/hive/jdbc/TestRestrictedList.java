7a1512656624b5e33a4aded53c3f988abac45b4f
Antal Sinkovits <asinkovits@cloudera.com>
2018-06-11 12:39:42 +0200

    Revert "CDH-69557: HIVE-18248: Clean up parameters (Janaki Lahorani, reviewed by Sahil Takiar)"
    This reverts commit 8a82acadb359940e8c928aad6b081725f371df91.
    
    Change-Id: If22ad6c4a7a735448d050048f7aa20c48a295d3b

--- itests/hive-unit/src/test/java/org/apache/hive/jdbc/TestRestrictedList.java
+++ itests/hive-unit/src/test/java/org/apache/hive/jdbc/TestRestrictedList.java
@@ -87,8 +87,6 @@ public class TestRestrictedList {
     addToExpectedRestrictedMap("hive.spark.client.rpc.server.port");
     addToExpectedRestrictedMap("hive.spark.client.rpc.sasl.mechanisms");
     addToExpectedRestrictedMap("spark.home");
-    addToExpectedRestrictedMap("bonecp.test");
-    addToExpectedRestrictedMap("hikari.test");
   }
 
   @AfterClass
