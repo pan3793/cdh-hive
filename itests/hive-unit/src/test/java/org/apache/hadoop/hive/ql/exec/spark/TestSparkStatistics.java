963c3d9c82454890dade79b8a8895a825dff362b
stakiar <stakiar@cloudera.com>
2018-07-10 12:04:35 -0500

    CLOUDERA-BUILD: CDH-70508: Fixing TestSparkStatistics
    Change-Id: I78da4d4b7c5ccd277b7589292eb698dd86ea93b4

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/exec/spark/TestSparkStatistics.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/exec/spark/TestSparkStatistics.java
@@ -79,7 +79,7 @@ public class TestSparkStatistics {
       List<SparkStatistic> sparkStats = Lists.newArrayList(sparkTask.getSparkStatistics()
               .getStatisticGroup(SparkStatisticsNames.SPARK_GROUP_NAME).getStatistics());
 
-      Assert.assertEquals(26, sparkStats.size());
+      Assert.assertEquals(25, sparkStats.size());
 
       Map<String, String> statsMap = sparkStats.stream().collect(
               Collectors.toMap(SparkStatistic::getName, SparkStatistic::getValue));
