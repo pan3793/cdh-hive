bbbb4f014206041ce79c86b2d44f5fe36b9b7acd
Adam Holley <aholley@cloudera.com>
2018-11-15 10:35:12 -0600

    CDH-75556: HMS schema tool does not upgrade from CDH 6.0->6.x releases
    Removed invalid test for version comparison.
    
    Testing:
    - Added test to compare various versions.
    
    Change-Id: I7563b46a2413c880e3692c230644d1f3c4b447e8
    (cherry picked from commit 61bf4634838269e190efa0d43ebcf6ec4ed75d27)

--- metastore/src/test/org/apache/hadoop/hive/metastore/TestCDHMetaStoreSchemaFactory.java
+++ metastore/src/test/org/apache/hadoop/hive/metastore/TestCDHMetaStoreSchemaFactory.java
@@ -34,4 +34,50 @@ public class TestCDHMetaStoreSchemaFactory {
     Assert.assertTrue("Unexpected instance type of the class MetaStoreSchemaInfo",
       metastoreSchemaInfo instanceof CDHMetaStoreSchemaInfo);
   }
+
+  // https://jira.cloudera.com/browse/CDH-75556
+  @Test
+  public void testCdhVersionChanges() {
+    // Ensure the compareTo() method works properly.
+    String [] versionStrings = {"1.1.0-cdh5.15.0", "1.1.0-cdh5.16.0", "2.1.0-cdh6.0.0",
+        "2.1.0-cdh6.1.0", "2.1.0-cdh6.1.1"};
+    CDHMetaStoreSchemaInfo.CDHVersion v1;
+    CDHMetaStoreSchemaInfo.CDHVersion v2;
+    for(int idx=0; idx<versionStrings.length-1; idx++) {
+      v1 = new CDHMetaStoreSchemaInfo.CDHVersion(versionStrings[idx]);
+      v2 = new CDHMetaStoreSchemaInfo.CDHVersion(versionStrings[idx+1]);
+      Assert.assertEquals(v1.toString() + "->" + v2.toString(), -1, v1.compareTo(v2));
+      Assert.assertEquals(v1.toString() + "->" + v2.toString(), 0, v1.compareTo(v1));
+      Assert.assertEquals(v1.toString() + "->" + v2.toString(), 0, v2.compareTo(v2));
+      Assert.assertEquals(v1.toString() + "->" + v2.toString(), 1, v2.compareTo(v1));
+    }
+
+    for(int idx=0; idx<versionStrings.length-1; idx++) {
+      v1 = new CDHMetaStoreSchemaInfo.CDHVersion(versionStrings[idx], true);
+      v2 = new CDHMetaStoreSchemaInfo.CDHVersion(versionStrings[idx+1], true);
+      if (versionStrings[idx].contains("cdh6.1")) {
+        Assert.assertEquals(v1.toString() + "->" + v2.toString(), 0, v1.compareTo(v2));
+        Assert.assertEquals(v1.toString() + "->" + v2.toString(), 0, v1.compareTo(v1));
+        Assert.assertEquals(v1.toString() + "->" + v2.toString(), 0, v2.compareTo(v2));
+        Assert.assertEquals(v1.toString() + "->" + v2.toString(), 0, v2.compareTo(v1));
+      } else {
+        Assert.assertEquals(v1.toString() + "->" + v2.toString(), -1, v1.compareTo(v2));
+        Assert.assertEquals(v1.toString() + "->" + v2.toString(), 0, v1.compareTo(v1));
+        Assert.assertEquals(v1.toString() + "->" + v2.toString(), 0, v2.compareTo(v2));
+        Assert.assertEquals(v1.toString() + "->" + v2.toString(), 1, v2.compareTo(v1));
+
+      }
+    }
+
+    // Check for versions of the form cdh6.x
+    v1 = new CDHMetaStoreSchemaInfo.CDHVersion("1.1.0-cdh5.15.0");
+    v2 = new CDHMetaStoreSchemaInfo.CDHVersion("2.1.0-cdh6.x");
+    Assert.assertEquals(v1.toString() + "->" + v2.toString(), -1, v1.compareTo(v2));
+    Assert.assertEquals(v2.toString() + "->" + v1.toString(), 1, v2.compareTo(v1));
+
+    v1 = new CDHMetaStoreSchemaInfo.CDHVersion("2.1.0-cdh6.1.0");
+    v2 = new CDHMetaStoreSchemaInfo.CDHVersion("2.1.0-cdh6.x");
+    Assert.assertEquals(v1.toString() + "->" + v2.toString(), 0, v1.compareTo(v2));
+    Assert.assertEquals(v2.toString() + "->" + v1.toString(), 0, v2.compareTo(v1));
+  }
 }
